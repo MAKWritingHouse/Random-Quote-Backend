de2b7dca4a1818c92f450fd02ffeef6b834ef1c2
MAK Writing House <134236495+MAKWritingHouse@users.noreply.github.com>
2023-08-24 00:25:58 +0500

    refactor code
    refactor code
    
    Co-Authored-By: Mohammad Moiz Ali <38657358+makstyle119@users.noreply.github.com>
    Co-Authored-By: MAK Development House <142262415+MAKDevelopmentHouse@users.noreply.github.com>

--- src/random-quote/service/random-quote.service.ts
+++ src/random-quote/service/random-quote.service.ts
@@ -3,11 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class RandomQuoteService {
   async getAll() {
-    const quotes = [];
-    await fetch('https://type.fit/api/quotes')
-      .then((res) => res.json())
-      .then((data) => quotes.push(...data));
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    return quotes[randomIndex];
+    const quotes = await fetch('https://type.fit/api/quotes').then((res) =>
+      res.json(),
+    );
+    return quotes[Math.floor(Math.random() * quotes.length)];
   }
 }
